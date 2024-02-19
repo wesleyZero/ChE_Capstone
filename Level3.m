88fce3d85b4232631d47976d7591ad6c78942aab
Wesley Johanson <wesleyJohansonZero@gmail.com>
2024-02-19 12:15:36 -0800

    isaiah confirmed the equations. updating the repo because its getting a clittle complicated. I'm adding the anon functions to make the code cleaner

--- Level3.m
+++ Level3.m
@@ -20,24 +20,32 @@ FC2H6 = 200;	% [	kta ]
 % Molar mass
 M_C2H6 = 28.05;	% [ g / mol ]
 
+% Unit conversions 
+metricTons_to_kiloton = 1000;
+
 
 
 % SYSTEM OF EQUARTIONS (EXTENT OF RXN)_____________________________________
 
-A = [s1-1	,s1		,s1+1;
+A = @(s1, s2)...
+	[s1-1	,s1		,s1+1;
      s2		,s2-1	,s2;
      1		,2		,1	];
 b = [0;		0;		FC2H6];
 
 % FLOWRATE FUNCTIONS_______________________________________________________
 
-P_H2 = @(xi_1) xi_1;
-P_CH4 = @(xi_2) xi_2;				% Verify with squad about this equation
-P_C2H4 = @(xi_1, xi_3) xi_1 - xi_3;
-P_C3H8 = @(xi_2) xi_2;
-P_C4H10 = @(xi_3) xi_3;
+P_H2 = @(xi_1)			xi_1;
+P_CH4 = @(xi_2)			xi_2;		% Verify with squad about this equation
+P_C2H4 = @(xi_1, xi_3)	xi_1 - xi_3;
+P_C3H8 = @(xi_2)		xi_2;
+P_C4H10 = @(xi_3)		xi_3;
 
+% VALIDITY EQUATIONS_______________________________________________________
 
+% Makes sure the flowrates are physically possible
+validSolution = @(p_h2, p_ch4, p_c2h4, p_c3h8, p_c4h10) ...
+	
 
 % SCRIPT___________________________________________________________________
 
@@ -47,18 +55,18 @@ P_C4H10 = @(xi_3) xi_3;
 
 for s1 = linspace(S1_MIN, S1_MAX, S1_POINTS)
 	for s2 = linspace(S2_MIN, S2_MAX, S2_POINTS)
+		
 		% Solve for extents of reaction
-		xi = A \ b; 
+		xi = A(s1, s2) \ b; 
+		disp("xi  is")
+		xi 
 
 		% Calculate the flow rates of each species
-		P_H2(xi(1))
-		P_CH4(xi(2))
-		P_C2H4(xi(1), xi(3))
-		P_C3H8(xi(2))
-		P_C4H10(xi(3))
-
-
-
+		p_h2 = P_H2(xi(1));
+		p_ch4 = P_CH4(xi(2));
+		p_c2h4 = P_C2H4(xi(1), xi(3));
+		p_c3h8 = P_C3H8(xi(2));
+		p_c4h10 = P_C4H10(xi(3));
 
 	end 
 end 
