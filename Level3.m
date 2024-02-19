43e7510754425d97f1d45ca9344fcad7a1890652
Wesley Johanson <wesleyJohansonZero@gmail.com>
2024-02-19 11:34:56 -0800

    working on the code. Got the system of equations in, writing the anonymouse functions for the flow rates. There is an inconsistency with isaiahs calculations though. We'll have an update on that today

--- Level3.m
+++ Level3.m
@@ -11,12 +11,21 @@ S2_MIN = 0.05;
 S2_MAX = 0.95;
 S2_POINTS = 50;
 
+% SYSTEM OF EQUARTIONS (EXTENT OF RXN)_____________________________________
+
+A = [s1-1	,s1		,s1+1;
+     s2		,s2-1	,s2;
+     1		,2		,1	];
+b = [0;		0;		FC2H6];
+
+% FLOWRATE FUNCTIONS_______________________________________________________
+
+
 
 % SCRIPT___________________________________________________________________
 
-% Code iterates through each value of S1 and S2 to find the economic
+% Iterates through each value of selectivities S1 and S2 to find the economic
 % potential for different reaction conditions 
-
 
 
 for s1 = linspace(S1_MIN, S1_MAX, S1_POINTS)
