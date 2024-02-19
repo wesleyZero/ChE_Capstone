263fbad627d956697b3cb8caee009543266b0217
Wesley Johanson <wesleyJohansonZero@gmail.com>
2024-02-19 11:49:44 -0800

    working on caclulating flow rates ...

--- Level3.m
+++ Level3.m
@@ -1,15 +1,26 @@
 clc; clear; close all; 
 
 
+
+
 % CONSTANTS________________________________________________________________
 
+% Plotting 
 S1_MIN = 0.05;
 S1_MAX = 0.95;
-S1_POINTS = 50;
+S1_POINTS = 4;
 
 S2_MIN = 0.05;
 S2_MAX = 0.95;
-S2_POINTS = 50;
+S2_POINTS = 4;
+
+% Design params
+FC2H6 = 200;	% [	kta ]
+
+% Molar mass
+M_C2H6 = 28.05;	% [ g / mol ]
+
+
 
 % SYSTEM OF EQUARTIONS (EXTENT OF RXN)_____________________________________
 
@@ -19,6 +30,12 @@ A = [s1-1	,s1		,s1+1;
 b = [0;		0;		FC2H6];
 
 % FLOWRATE FUNCTIONS_______________________________________________________
+
+P_H2 = @(xi_1) xi_1;
+P_CH4 = @(xi_2) xi_2;				% Verify with squad about this equation
+P_C2H4 = @(xi_1, xi_3) xi_1 - xi_3;
+P_C3H8 = @(xi_2) xi_2;
+P_C4H10 = @(xi_3) xi_3;
 
 
 
@@ -30,6 +47,18 @@ b = [0;		0;		FC2H6];
 
 for s1 = linspace(S1_MIN, S1_MAX, S1_POINTS)
 	for s2 = linspace(S2_MIN, S2_MAX, S2_POINTS)
-		fprintf("%f\n", s1)
+		% Solve for extents of reaction
+		xi = A \ b; 
+
+		% Calculate the flow rates of each species
+		P_H2(xi(1))
+		P_CH4(xi(2))
+		P_C2H4(xi(1), xi(3))
+		P_C3H8(xi(2))
+		P_C4H10(xi(3))
+
+
+
+
 	end 
 end 
