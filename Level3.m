ded34ec73652d0b856b88fe915912d848f7e9033
Wesley Johanson <wesleyJohansonZero@gmail.com>
2024-02-19 10:23:05 -0800

    Getting some of the initial parts of the script done. Just got the for loop set up. Haven't implemented the code yet

--- Level3.m
+++ Level3.m
@@ -1,4 +1,26 @@
 clc; clear; close all; 
 
 
-disp("test")
+% CONSTANTS________________________________________________________________
+
+S1_MIN = 0.05;
+S1_MAX = 0.95;
+S1_POINTS = 50;
+
+S2_MIN = 0.05;
+S2_MAX = 0.95;
+S2_POINTS = 50;
+
+
+% SCRIPT___________________________________________________________________
+
+% Code iterates through each value of S1 and S2 to find the economic
+% potential for different reaction conditions 
+
+
+
+for s1 = linspace(S1_MIN, S1_MAX, S1_POINTS)
+	for s2 = linspace(S2_MIN, S2_MAX, S2_POINTS)
+		fprintf("%f\n", s1)
+	end 
+end 
